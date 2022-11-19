0aff1cbd304802c4b55728bc1ab20d80193b197c
mdd-star <1647728631@qq.com>
2022-11-19 15:35:25 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+<!--  <img alt="Vue logo" src="./assets/logo.png">-->
+<!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <LoginIndex style="width:30%;margin-top:15%;margin-left:30%"></LoginIndex>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import LoginIndex from './components/LoginIndex.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    LoginIndex
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/LoginIndex.vue
@@ -0,0 +1,119 @@
+<template>
+  <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+    <el-form-item label="用户名" prop="name">
+      <el-input type="text" v-model="ruleForm.name" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="密码" prop="pass">
+      <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="确认密码" prop="checkPass">
+      <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="年龄" prop="age">
+      <el-input v-model.number="ruleForm.age"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+      <el-button @click="resetForm('ruleForm')">重置</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  name: "LoginIndex",
+  data() {
+    var checkName = (rule, value, callback) => {
+      const check = /\d{15}/
+      console.log(typeof value);
+      if (!value) {
+        return callback(new Error('用户名不能为空'));
+      }
+      setTimeout(() => {
+        if (value.length !== 15) {
+          callback(new Error('用户名长度位15位'));
+        } else {
+          if (!check.test(value)) {
+            callback(new Error('请输入数字'));
+          } else {
+            callback();
+          }
+        }
+      }, 1000);
+    };
+    var checkAge = (rule, value, callback) => {
+      console.log(typeof value);
+      if (!value) {
+        return callback(new Error('年龄不能为空'));
+      }
+      setTimeout(() => {
+        if (!Number.isInteger(value)) {
+          callback(new Error('请输入数字值'));
+        } else {
+          if (value < 18) {
+            callback(new Error('必须年满18岁'));
+          } else {
+            callback();
+          }
+        }
+      }, 1000);
+    };
+    var validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'));
+      } else {
+        if (this.ruleForm.checkPass !== '') {
+          this.$refs.ruleForm.validateField('checkPass');
+        }
+        callback();
+      }
+    };
+    var validatePass2 = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'));
+      } else if (value !== this.ruleForm.pass) {
+        callback(new Error('两次输入密码不一致!'));
+      } else {
+        callback();
+      }
+    };
+    return {
+      ruleForm: {
+        name: '',
+        pass: '',
+        checkPass: '',
+        age: ''
+      },
+      rules: {
+        name: [
+          {validator: checkName, trigger: 'blur'}
+        ],
+        pass: [
+          {validator: validatePass, trigger: 'blur'}
+        ],
+        checkPass: [
+          {validator: validatePass2, trigger: 'blur'}
+        ],
+        age: [
+          {validator: checkAge, trigger: 'blur'}
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+}
+</script>
